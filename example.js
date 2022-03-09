d0126d767709c1e0bad4c931dc5cdacd5573d93a
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2022-03-09 11:32:51 -0500

    Create example.js

---
+++ example.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+const url = "https://example.com";
+
+const getData = async url => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData(url);
