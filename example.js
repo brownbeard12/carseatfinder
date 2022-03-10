3bc9a8e3a63bc9e5de65d28fe7e9984dcb3d3967
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2022-03-10 07:21:08 -0500

    scraping

--- example.js
+++ example.js
@@ -2,16 +2,26 @@
 const pup = require('puppeteer');
 
 (async function scrape() {
+  const timer = 100
   const browser = await pup.launch({ headless: false })
   const page = await browser.newPage();
-  page.goto('https://www.chiccousa.com/shop-our-products/car-seats/infant/');
-  page.waitForTimeout(500)
+  await page.goto('https://www.chiccousa.com/shop-our-products/car-seats/infant/');
+  await page.waitForTimeout(timer)
     .then(() => page.mouse.wheel({ deltaY: 500 })
-      .then(() => page.waitForTimeout(500)
+      .then(() => page.waitForTimeout(timer)
         .then(() => page.mouse.wheel({ deltaY: 2000 })))
-      .then(() => page.waitForTimeout(500)
+      .then(() => page.waitForTimeout(timer)
         .then(() => page.mouse.wheel({ deltaY: 1000 })))
-      .then(() => page.waitForTimeout(500)
-        .then(() => page.mouse.wheel({ deltaY: 2000 }))));
+      .then(() => page.waitForTimeout(timer)
+        .then(() => page.mouse.wheel({ deltaY: 3000 })))
+        .then(() => page.waitForTimeout(timer*2)));
+
+  let prods = await page.evaluate(() => {
+    let items = document.body.querySelectorAll('div.product-name')
+    return items;
+
+  })
+  console.log(prods)
+  await browser.close()
 
 })();
