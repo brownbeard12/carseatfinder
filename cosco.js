01e250116a6bba386d9cf56194009326f43fb6c4
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2022-03-18 17:32:17 -0400

    cosco

--- cosco.js
+++ cosco.js
@@ -10,9 +10,21 @@ async function scrape() {
 
       const doc = new JSDOM(data);
 
-      const items = doc.window.document.querySelectorAll("li.product-item")[0].querySelector('a.product-item-link').getAttribute('href');
+      const items = doc.window.document.querySelectorAll("li.product-item");
+
+      let _items = Object.values(items).map(em => {
+        return {
+          prod_url: em.querySelector('a.product-item-link').getAttribute('href'),
+          // item_id: 
+          // prod_id: 
+          // name: 
+          // category: 
+          // img_url: 
+          // price: 
+        }
+      })
       
-      return items;
+      return _items;
   });
   console.log(response)
 };
