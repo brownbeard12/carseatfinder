a5f891b9b90ca1524e5c5e07af23fad03f01dd26
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2022-08-29 15:17:27 -0400

    status

--- functions/index.js
+++ functions/index.js
@@ -824,37 +824,37 @@ exports.evenflo = functions.runWith({ memory: '2GB' }).https.onRequest((request,
 exports.updateStatus = functions.https.onRequest((request, response) => {
 
   require('dotenv').config()
-const faunadb = require('faunadb')
-const q = faunadb.query
-const utils = require('./utils.js')
+  const faunadb = require('faunadb')
+  const q = faunadb.query
+  const utils = require('./utils.js')
 
-//Setup Fauna
-const secret = process.env.FAUNADB_SECRET
-let endpoint = process.env.FAUNADB_ENDPOINT
+  //Setup Fauna
+  const secret = process.env.FAUNADB_SECRET
+  let endpoint = process.env.FAUNADB_ENDPOINT
 
-if (typeof secret === 'undefined' || secret === '') {
-  console.error('key not set')
-  process.exit(1)
-}
+  if (typeof secret === 'undefined' || secret === '') {
+    console.error('key not set')
+    process.exit(1)
+  }
 
-if (!endpoint) endpoint = 'https://db.us.fauna.com'
+  if (!endpoint) endpoint = 'https://db.us.fauna.com'
 
-let mg, domain, port, scheme
-if ((mg = endpoint.match(/^(https?):\/\/([^:]+)(:(\d+))?/))) {
-  scheme = mg[1] || 'https'
-  domain = mg[2] || 'db.us.fauna.com'
-  port = mg[4] || 443
-}
+  let mg, domain, port, scheme
+  if ((mg = endpoint.match(/^(https?):\/\/([^:]+)(:(\d+))?/))) {
+    scheme = mg[1] || 'https'
+    domain = mg[2] || 'db.us.fauna.com'
+    port = mg[4] || 443
+  }
 
-const client = new faunadb.Client({
-  secret: secret,
-  domain: domain,
-  port: port,
-  scheme: scheme,
-})
+  const client = new faunadb.Client({
+    secret: secret,
+    domain: domain,
+    port: port,
+    scheme: scheme,
+  })
 
-//Call query
+  //Call query
 
 
-utils.updateStatus(client, q);
+  utils.updateStatus(client, q);
 })
